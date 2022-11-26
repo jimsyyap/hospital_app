f5843402a300e248f939f7fb4a47a8ad3e763616
jimsyyap <jimsyyap@gmail.com>
2022-11-26 16:05:42 +1100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,15 @@
 
 @section('content')
 
-    <h2>Content goes here</h2>
+    <div class="container-lg" style="margin: 0 auto">
+        <div class="row mt-5">
+            <div class="col-lg-4 col-md-4 col-sm-12 text-center mb-3 mx-5">
+                <div class="card" style="width: 28rem">
+                    <img src="" alt="">
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
